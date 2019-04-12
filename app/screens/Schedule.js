08ff12ba78f62703571bd9ffb2b9e8044de3a321
Mateus Sousa <mateus.sousa@deway.com.br>
2019-04-12 09:57:29 -0300

    first commit

---
+++ app/screens/Schedule.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ImageBackground, FlatList } from 'react-native';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import todayImage from '../../assets/imgs/today.jpg';
+import commonStyles from '../utils/commonStyles';
+import Task from '../components/Task'
+
+export default class componentName extends Component {
+    state = {
+        tasks: [
+            {
+                id: Math.random(),
+                description: 'Comprar o Curso React Native',
+                estimateAt: new Date(),
+                doneAt: new Date()
+            },
+            {
+                id: Math.random(),
+                description: 'Concluir Curso',
+                estimateAt: new Date(),
+                doneAt: null
+            }
+        ]
+    }
+
+    toggleTask = id => {
+        const tasks = this.state.tasks.map(task => {
+            if (task.id === id) {
+                task = { ...task }
+                task.doneAt = task.doneAt ? null : new Date();
+            }
+            return task
+        })
+        this.setState({ tasks })
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground
+                    source={todayImage}
+                    style={styles.background}
+                >
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>
+                            {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskContainer}>
+                    <FlatList
+                        data={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({ item }) =>
+                            <Task {...item} toggleTask={this.toggleTask} />}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
+    },
+    taskContainer: {
+        flex: 7,
+    }
+})
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import Schedule from './app/screens/Schedule'
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => Schedule);
